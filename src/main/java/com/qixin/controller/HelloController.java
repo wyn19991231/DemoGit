d367e2f13cddfa7c9b22a6253f59948451b85180
wuyanan <3082992626@qq.com>
2018-11-20 13:24:28 +0800

    这是dGit项目的修改,为了区分项目

--- src/main/java/com/qixin/controller/HelloController.java
+++ src/main/java/com/qixin/controller/HelloController.java
@@ -4,10 +4,10 @@ public class HelloController {
 
 	public void hello() {
 		System.out.println("hello");
+		System.out.println("demoGit");
 	}
 	
-	//这是一个用户支付功能，不能修改
-	public void demoGit() {
-		System.out.println("demoGit的支付功能");
+	public void dGit() {
+		System.out.println("dGit的登陆功能");
 	}
 }
