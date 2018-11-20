338d7b988e80d9aa4c77c3015aa8c6e2e210d8f7
wuyanan <3082992626@qq.com>
2018-11-20 13:28:31 +0800

    增加了DemoGit功能

--- src/main/java/com/qixin/controller/HelloController.java
+++ src/main/java/com/qixin/controller/HelloController.java
@@ -4,10 +4,12 @@ public class HelloController {
 
 	public void hello() {
 		System.out.println("hello");
+		System.out.println("hellojj");
 	}
 	
 	//这是一个用户支付功能，不能修改
 	public void demoGit() {
 		System.out.println("demoGit的支付功能");
 	}
+	
 }
