e776b918f7732dd88b8506a367e88dec59787f61
wuyanan <3082992626@qq.com>
2018-11-20 16:13:18 +0800

    xxx提交

--- src/main/java/com/qixin/controller/HelloController.java
+++ src/main/java/com/qixin/controller/HelloController.java
@@ -5,6 +5,8 @@ public class HelloController {
 	public void hello() {
 		System.out.println("hello");
 		System.out.println("demoGit");
+		System.out.println("demoGitxxxx");
+		System.out.println("demoGitxxx");
 	}
 	
 	//这是一个用户支付功能，不能修改
@@ -18,5 +20,9 @@ public class HelloController {
 	//这是用户充值功能
 	public void DemoGit() {
 		System.out.println("DemoGit的充值功能");
+	}
+	
+	public void demoGit02() {
+		System.out.println("demoGit02");
 	}
 }
